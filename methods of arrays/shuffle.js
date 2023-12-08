16882dae59c99fbff962f1ef8033f9870d326a18
Oleg <olegashepelev@gmail.com>
2023-12-08 21:41:34 +0600

    Complete shuffle.js

--- methods of arrays/shuffle.js
+++ methods of arrays/shuffle.js
@@ -3,6 +3,17 @@
  порядке. Функция должна изменять исходный массив, а не возвращать новый. Например, shuffle([1, 2, 3, 4, 5]) может
  вернуть [3, 1, 5, 4, 2].
  * */
-function shuffle(arr1, arr2) {
-    // Your code
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        // Генерируем случайный индекс от 0 до i
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+
+        // Меняем местами текущий элемент с элементом, выбранным случайным образом
+        let temp = arr[i];
+        arr[i] = arr[randomIndex];
+        arr[randomIndex] = temp;
+    }
+    return arr;
 }
+
+console.log(shuffle([1, 2, 3, 4, 5]));

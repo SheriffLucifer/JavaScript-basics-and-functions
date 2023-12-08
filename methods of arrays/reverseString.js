e610ba44c2e4e3ee9b33868c907eaed4693e4158
Oleg <olegashepelev@gmail.com>
2023-12-08 21:35:13 +0600

    Complete reverseString.js

--- methods of arrays/reverseString.js
+++ methods of arrays/reverseString.js
@@ -3,6 +3,8 @@
  * @param {string} str - входная строка
  * @return {string} - строка, обращенная задом наперед
  */
-function reverseString(str) {
-    // ваш код здесь
+function reverseString(inputString) {
+    return inputString.split("").reverse().join("");
 }
+
+console.log(reverseString("Tenet is the middle"));

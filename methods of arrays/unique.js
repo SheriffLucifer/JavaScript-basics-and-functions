59218d6bf19e033f50321bf3a3a075b22f247325
Oleg <olegashepelev@gmail.com>
2023-12-08 21:45:27 +0600

    Complete unique.js

--- methods of arrays/unique.js
+++ methods of arrays/unique.js
@@ -4,5 +4,27 @@
  * @return {string[]} - массив строк, содержащий только уникальные значения
  */
 function uniqueStrings(arr) {
-    // ваш код здесь
+    let uniqueArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentString = arr[i];
+
+        // Проверяем, есть ли текущая строка в уникальном массиве
+        if (!uniqueArray.includes(currentString)) {
+            uniqueArray.push(currentString);
+        }
+    }
+
+    return uniqueArray;
 }
+
+console.log(
+    uniqueStrings([
+        "dsagadfg",
+        "2325615156",
+        "dsagadfg",
+        "adlvblhjr,bfhja,bfhjd",
+        "2325615156",
+        "adlvblhjr,bfhja,bfhjd",
+    ])
+);

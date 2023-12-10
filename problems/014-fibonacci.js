36ab844a24c120c96f60ae5fb19b36f90ef51cf7
Oleg <olegashepelev@gmail.com>
2023-12-10 22:09:26 +0600

    Complete task 014-fibonacci

--- problems/014-fibonacci.js
+++ problems/014-fibonacci.js
@@ -17,7 +17,22 @@
  * @returns {number}
  */
 function fibonacci(n) {
-    return undefined;
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        let a = 0;
+        let b = 1;
+
+        for (let i = 2; i <= n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
 }
 
 module.exports = fibonacci;

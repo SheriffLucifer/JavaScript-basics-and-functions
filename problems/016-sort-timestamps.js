f5bdd562d38fa4e3c52ec91e6973d892ef0cc803
Oleg <olegashepelev@gmail.com>
2023-12-11 22:57:05 +0600

    Complete task 016-sort-timestamps

--- problems/016-sort-timestamps.js
+++ problems/016-sort-timestamps.js
@@ -8,8 +8,23 @@
  * @param {string[]} list массив временных моментов представленных в виде строк в формате 'HH:MM:SS'
  * @returns {string[]} отсортированный по возрастанию массив временных моментов
  */
+
 function sortTimestamps(list) {
-    return list.sort();
+    list.sort((time1, time2) => {
+        const [h1, m1, s1] = time1.split(":").map(Number);
+        const [h2, m2, s2] = time2.split(":").map(Number);
+
+        // Сравниваем по часам, затем по минутам, затем по секундам
+        if (h1 !== h2) {
+            return h1 - h2;
+        } else if (m1 !== m2) {
+            return m1 - m2;
+        } else {
+            return s1 - s2;
+        }
+    });
+
+    return list;
 }
 
 module.exports = sortTimestamps;
